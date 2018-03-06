49f70c5033ebd841faca36da859be7462a88322f
FingerArt <george@chengguo.io>
2018-03-06 14:33:06 +0800

    Update Doc

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
-# OHttp
+# ExHttp
 
-A simple android http libray.
+A lightweight URLConnection HTTP wrapper library for android.
+
+## Sample
+
+``` java
+//Sync
+ExHttp.get()
+      .url("http://httpbin.org/get")
+      .addQuery("k1", "v1")
+      .addHeader("hello", "world")
+      .build()
+      .execute();
+
+//Async
+IHttpRequestCallback callback = 
+        new IHttpRequestCallback() {
+            @Override
+            public void onStart() {
+            }
+
+            @Override
+            public void onSuccess(Response response) {
+            }
+
+            @Override
+            public void onError(Exception e) {
+            }
+
+            @Override
+            public void onFinish() {
+            }
+        };
+ExHttp.get()
+      .url("http://httpbin.org/kkkkk")
+      .addQuery("q1", "qv1")
+      .build()
+      .execute(callback);
+```
--- sample/src/main/java/io/chengguo/exhttp/sample/MainActivity.java
+++ sample/src/main/java/io/chengguo/exhttp/sample/MainActivity.java
@@ -1,15 +1,7 @@
 package io.chengguo.exhttp.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-
-import io.chengguo.exhttp.ExHttp;
-import io.chengguo.exhttp.GHttpSampleRequestCallback;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,30 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            InputStream ssl = getAssets().open("cer/ssl.p12");
-            ExHttp.post()
-                    .url("http://10.168.1.242:8888/webapi/users/login")
-                    .json("{\"username\":\"zhangming\",\"password\":\"123456\"}")
-                    .build()
-                    .execute(new GHttpSampleRequestCallback() {
-                @Override
-                public void onSuccess(int responseCode, InputStream inputStream, HttpURLConnection connection) {
-                    Log.d(TAG, "onSuccess() called with: responseCode = [" + responseCode + "], inputStream = [" + inputStream + "], connection = [" + connection + "]");
-                }
-
-                @Override
-                public void onFinish() {
-                    Log.d(TAG, "onFinish() called");
-                }
-
-                @Override
-                public void onError(Exception e) {
-                    e.printStackTrace();
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
